fed9966ca18eb2c8345611a49773cfda72653046
Gabriel Herrera Zarate <raona_ggherrera@sacyr.com>
2024-03-13 21:21:59 +0100

    new json vercel with endpoint configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,57 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/contact",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "false" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
+          },
+        ],
+      },
+      {
+        source: "/api/order",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "false" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
+          },
+        ],
+      },
+      {
+        source: "/api/payment",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "false" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
